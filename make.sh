b9efafc5c893dc316a41ac24ad4f3a2f21bcae31
dfurneau <dfurneau@student.42.fr>
2021-12-20 13:49:58 +0400

    remove make.sh

--- make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/Applications/Docker.app/Contents/MacOS/Docker &
-
-rm -rf ~/Library/Containers/com.docker.docker &&
-ln -s ~/goinfre/docker ~/Library/Containers/com.docker.docker &&
-
-
-./build
